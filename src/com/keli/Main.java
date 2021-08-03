8e58388e3662a60f57c629b3892e3db6e1fcd4bb
keliqiao <qiaokeliqiao@qq.com>
2021-08-03 10:17:09 +0800

    注释

--- src/com/keli/Main.java
+++ src/com/keli/Main.java
@@ -1,9 +1,15 @@
 package com.keli;
 
+/**
+ * javadoc注释
+ */
+/*
+普通注释
+ */
+    //单行注释
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("helloworld");
         System.out.print("helloworld");
     }
 }
